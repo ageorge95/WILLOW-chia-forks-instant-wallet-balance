f08c47084fcbe34ab98b7e867d12925e9f633bb9
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-03-12 08:40:03 +0200

    added support for XJK - joker

--- _00_config.py
+++ _00_config.py
@@ -1,6 +1,9 @@
 from os import path
 
-initial_config = {'assets': {'GL': {'db_filepath': '{userdir}\\.gold\\mainnet\\db\\blockchain_v1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
+initial_config = {'assets': {'XJK': {'db_filepath': '{userdir}\\.joker\\mainnet\\db\\blockchain_v1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
+                                 'denominator': 100000000,
+                                 'friendly_name': 'joker'},
+                          'GL': {'db_filepath': '{userdir}\\.gold\\mainnet\\db\\blockchain_v1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
                                  'denominator': 1000000000000,
                                  'friendly_name': 'gold'},
                           'BPX': {'db_filepath': '{userdir}\\.bpx\\mainnet\\db\\blockchain_v2_mainnet.sqlite'.format(userdir=path.expanduser("~")),
