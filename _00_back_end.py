4849e874009055a08e8e6b132ea0aac952e557a6
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-03-12 09:10:13 +0200

    hotfix for joker-XJK

--- _00_back_end.py
+++ _00_back_end.py
@@ -6,6 +6,9 @@ def select_backend(coin):
     if coin in ['XCC']:
         from _00_back_end_XCC import WILLOW_back_end
         return WILLOW_back_end
+    if coin in ['XJK']:
+        from _00_back_end_XJK import WILLOW_back_end
+        return WILLOW_back_end
     else:
         from _00_back_end_XCH import WILLOW_back_end
         return WILLOW_back_end
