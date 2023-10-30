d860f89cef105364a21ab11cd8fd61a2e5e7d880
ageorge95 <arteni.george.daniel@gmail.com>
2023-10-30 17:46:56 +0200

    added install_helper.py

--- Install.bat
+++ Install.bat
@@ -1,20 +1,31 @@
 @echo off
 
+set python=%1
+IF "%python%"=="" set python="python"
+
 set root=%cd%
 
 :: installation
 
 git submodule update --progress --init --recursive --force
-mklink /j chia chia_blockchain\chia
 
-python -m venv venv
+%python% -m venv venv
 :: Windows doesn't allow the creation of symlinks without special priviledges, so hardlinks are created instead.
 mklink /h activate.bat venv\Scripts\activate.bat
 mklink /j venv\Scripts\chia chia_blockchain\chia
+mklink /j chia chia_blockchain\chia
 mklink /j venv\Scripts\WeepingWillow WeepingWillow
-mklink /j venv\Scripts\media media
 
 call activate.bat
+
+python install_helper.py || goto :error
+goto :all_good
+
+: error
+pause
+exit
+
+: all_good
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
@@ -30,14 +41,16 @@ echo.
 echo Run 'activate' to activate WILLOW's Python virtual environment and
 echo 'deactivate' to, well, deactivate it.
 echo.
-echo Run 'gui_willow' to run the GUI.
+echo Run 'gui_willow' to run the GUI
 echo.
-echo Run 'willow -h' for further instructions.
+echo Run 'willow -h' to run the CLI interface
 echo.
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 rmdir /s /q %root%\build
 rmdir /s /q %root%\dist
 rmdir /s /q %root%\WILLOW.egg-info
+
+pause
 
 deactivate
