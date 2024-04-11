63763795d66dfc7d7269fdbb15a9f7589d3c1fd6
ageorge95 <arteni.george.daniel@gmail.com>
2024-04-11 16:44:14 +0300

    updated the default db path for BALL

--- WeepingWillow/config.py
+++ WeepingWillow/config.py
@@ -20,7 +20,7 @@ initial_config = {'assets': {'XXCH': {'db_filepath': '{userdir}\\.xxch\\mainnet\
                                       'denominator': 1000000000000,
                                       'friendly_name': 'two',
                                       'wallet_sk_derivation_port': [8444]},
-                             'BALL': {'db_filepath': '{userdir}\\.ball\\mainnet\\db\\blockchain_v2_mainnet.sqlite'.format(userdir=path.expanduser("~")),
+                             'BALL': {'db_filepath': '{userdir}\\.ball\\mainnet\\db\\blockchain_v2_r1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
                                       'denominator': 1000000000000,
                                       'friendly_name': 'ballcoin',
                                       'wallet_sk_derivation_port': [8444]},
